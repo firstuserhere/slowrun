9743ca90762fe15708cf41af73424c49140f6b76
Akshay Vegesna <akshay.vegesna@gmail.com>
2026-02-27 06:49:06 +0000

    Remove MuonAdamW

--- limited/train.py
+++ limited/train.py
@@ -3,7 +3,7 @@ Train a language model on ~100M tokens with val loss evaluation.
 Code is based on Nanochat (https://github.com/karpathy/nanochat), with modifications to support the slowrun setting.
 
 Usage:
-    torchrun --standalone --nproc_per_node=8 train.py
+    torchrun --standalone --nproc_per_node=8 limited/train.py
 """
 
 import os
@@ -315,8 +315,7 @@ class GPT(nn.Module):
             param_groups.append(dict(kind='muon', params=group_params, lr=MATRIX_LR,
                                      momentum=0.95, ns_steps=5, beta2=0.95, weight_decay=WEIGHT_DECAY))
 
-        Factory = DistMuonAdamW if ddp else MuonAdamW
-        optimizer = Factory(param_groups)
+        optimizer = DistMuonAdamW(param_groups)
         for group in optimizer.param_groups:
             group["initial_lr"] = group["lr"]
         return optimizer
@@ -394,71 +393,6 @@ def muon_step_fused(stacked_grads, stacked_params, momentum_buffer, second_momen
     wd = wd_t.to(g.dtype)
     mask = (g * stacked_params) >= 0
     stacked_params.sub_(lr * g + lr * wd * stacked_params * mask)
-
-
-class MuonAdamW(torch.optim.Optimizer):
-    """Single-GPU MuonAdamW optimizer."""
-    def __init__(self, param_groups):
-        super().__init__(param_groups, defaults={})
-        self._adamw_step_t = torch.tensor(0.0)
-        self._adamw_lr_t = torch.tensor(0.0)
-        self._adamw_beta1_t = torch.tensor(0.0)
-        self._adamw_beta2_t = torch.tensor(0.0)
-        self._adamw_eps_t = torch.tensor(0.0)
-        self._adamw_wd_t = torch.tensor(0.0)
-        self._muon_momentum_t = torch.tensor(0.0)
-        self._muon_lr_t = torch.tensor(0.0)
-        self._muon_wd_t = torch.tensor(0.0)
-        self._muon_beta2_t = torch.tensor(0.0)
-
-    def _step_adamw(self, group):
-        for p in group['params']:
-            if p.grad is None: continue
-            state = self.state[p]
-            if not state:
-                state['step'] = 0
-                state['exp_avg'] = torch.zeros_like(p)
-                state['exp_avg_sq'] = torch.zeros_like(p)
-            state['step'] += 1
-            self._adamw_step_t.fill_(state['step'])
-            self._adamw_lr_t.fill_(group['lr'])
-            self._adamw_beta1_t.fill_(group['betas'][0])
-            self._adamw_beta2_t.fill_(group['betas'][1])
-            self._adamw_eps_t.fill_(group['eps'])
-            self._adamw_wd_t.fill_(group['weight_decay'])
-            adamw_step_fused(p, p.grad, state['exp_avg'], state['exp_avg_sq'],
-                           self._adamw_step_t, self._adamw_lr_t, self._adamw_beta1_t,
-                           self._adamw_beta2_t, self._adamw_eps_t, self._adamw_wd_t)
-
-    def _step_muon(self, group):
-        params = group['params']
-        if not params: return
-        p = params[0]
-        state = self.state[p]
-        shape, device, dtype = p.shape, p.device, p.dtype
-        if "momentum_buffer" not in state:
-            state["momentum_buffer"] = torch.zeros(len(params), *shape, dtype=dtype, device=device)
-        if "second_momentum_buffer" not in state:
-            s = (len(params), shape[-2], 1) if shape[-2] >= shape[-1] else (len(params), 1, shape[-1])
-            state["second_momentum_buffer"] = torch.zeros(s, dtype=dtype, device=device)
-        red_dim = -1 if shape[-2] >= shape[-1] else -2
-        stacked_grads = torch.stack([p.grad for p in params])
-        stacked_params = torch.stack(params)
-        self._muon_momentum_t.fill_(group["momentum"])
-        self._muon_beta2_t.fill_(group["beta2"])
-        self._muon_lr_t.fill_(group["lr"] * max(1.0, shape[-2] / shape[-1])**0.5)
-        self._muon_wd_t.fill_(group["weight_decay"])
-        muon_step_fused(stacked_grads, stacked_params, state["momentum_buffer"],
-                       state["second_momentum_buffer"], self._muon_momentum_t, self._muon_lr_t,
-                       self._muon_wd_t, self._muon_beta2_t, group["ns_steps"], red_dim)
-        torch._foreach_copy_(params, list(stacked_params.unbind(0)))
-
-    @torch.no_grad()
-    def step(self):
-        for group in self.param_groups:
-            if group['kind'] == 'adamw': self._step_adamw(group)
-            elif group['kind'] == 'muon': self._step_muon(group)
-
 
 class DistMuonAdamW(torch.optim.Optimizer):
     """Distributed MuonAdamW with ZeRO-2 style sharding."""
